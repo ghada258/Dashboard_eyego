67fa13c871bc325cb1b79d07744a9d505cca1810
ghada <ghadaelsayed20@gmail.com>
2025-10-19 14:55:30 +0300

    download and pagination and search

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-
+import Chart from "@/components/Chart";
 const layout = ({children}) => {
   return (
+   
     <>
-     <Navbar/>
-     <Sidebar/>
-     <div>
-        {children}
-     </div>
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 flex justify-center items-center mt-15 pl-40">
+         {children}
+        </main>
+      </div>
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { RiLogoutBoxLine } from "react-icons/ri";
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
@@ -6,18 +10,20 @@ const Sidebar = () => {
      <aside className="fixed top-0 left-0 h-screen w-64 bg-[#23466bff] text-white shadow-lg flex flex-col justify-between">
 
       <nav className="flex-col gap-2 px-4 py-25 space-y-4 text-white">
-        <a href="/dashboard" className="flex text-lg items-center  p-2 rounded-lg text-white hover:bg-white hover:text-[#23466bff] transition">
-         Dashboard
-        </a>
-         <a href="/" className="flex text-lg items-center  p-2 rounded-lg text-white hover:bg-white hover:text-[#23466bff] transition">
-          Employees
-        </a>
+
+        <Link href="/dashboard" className="flex text-lg items-center gap-3 p-2 rounded-lg text-white hover:bg-white hover:text-[#23466bff] transition">
+         <span><LuLayoutDashboard /></span> <span>Dashboard</span>
+        </Link>
+         <Link href="/dashboard/employee" className="flex text-lg items-center gap-3 p-2 rounded-lg text-white hover:bg-white hover:text-[#23466bff] transition">
+         <span><MdOutlinePeopleAlt /> </span> <span>Employees</span>
+        </Link>
     
       </nav>
 
       <div className="p-4 border-t  border-white">
-        <button className="flex items-center gap-3 hover:text-[#23466bff] w-full p-2 rounded-lg hover:bg-white transition">
-           Logout
+        <button className="flex items-center gap-3 hover:text-[#23466bff] w-full text-lg p-2 rounded-lg hover:bg-white transition">
+         <span> <RiLogoutBoxLine /> </span> <span>Logout</span>
+          
         </button>
       </div>
     </aside>
