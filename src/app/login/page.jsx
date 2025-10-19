d3507eeb3412d5c24fbb44982b690ecfe0b00621
ghada <ghadaelsayed20@gmail.com>
2025-10-19 15:12:42 +0300

    authentication

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,33 @@
 "use client";
-import React from 'react';
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const page = () => {
         const router = useRouter();
-        const handleSubmit = (e) => {
-        e.preventDefault();
-        router.push("/dashboard");
-     };
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [error, setError] = useState("");
+
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            setError(""); 
+
+            try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users");
+            const users = await res.json();
+
+            const userExists = users.some((user) => user.email === email);
+
+            if (userExists) {
+                router.push("/dashboard");
+            } else {
+                setError("Invalid email or password");
+            }
+            } catch (err) {
+            console.error(err);
+            setError("Something went wrong. Please try again later.");
+            }
+        };
   return (
     <div className="bg-[url('/blue_background.svg')] bg-cover bg-center min-h-screen flex justify-center items-center">
        <div className='bg-white w-xl rounded-xl flex-col justify-center items-center text-center py-6 px-8'>
@@ -22,15 +42,17 @@ const page = () => {
             <label htmlFor="email" className="block text-left pl-2 text-gray-700 font-medium mb-1">
             Email Address
             </label>
-           <input type="email" id="email" placeholder="example@email.com" className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500"/>
+           <input type="email" id="email" placeholder="example@email.com" className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500"  value={email}
+              onChange={(e) => setEmail(e.target.value)}/>
           </div>
            <div className="w-full mb-4">
             <label htmlFor="email" className="block text-left pl-2 text-gray-700 font-medium mb-1">
             Password
             </label>
-           <input type="password" id="password" placeholder="************" className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500"/>
+           <input type="password" id="password" placeholder="************" className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500" value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
           </div>
-
+            {error && <p className="text-red-500 text-sm">{error}</p>}
            <button type="submit" className="w-full bg-[#23466bff] hover:bg-[#1C7EBB] text-white font-semibold py-2 rounded-lg mt-6" >
             Sign In
           </button>
